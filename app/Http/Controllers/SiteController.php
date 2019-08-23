b706884024ccbec5a87b0055d53cbf117dc3f19c
Ovie Tennyson <atomictenny@gmail.com>
2019-08-23 18:21:21 +0100

    Added status column to getStarted SIteController

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -167,6 +167,7 @@ class SiteController extends Controller
                 'first_name' => 'Nelly',
                 'last_name' => 'Onovwiona',
                 'phone' => '0908767878',
+                'status' => <i class="fas fa-repeat-1-alt    "></i>,
                 'role' => 5,
                 'verified' => 1,
                 'email_verified_at' => '2019-07-02 17:00:50',

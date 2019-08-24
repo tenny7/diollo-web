aae04273442596ea8080d5d3729e567b31c87cdc
Ovie Tennyson <atomictenny@gmail.com>
2019-08-24 19:31:27 +0100

    Modified UI/UX

--- resources/views/user/wallet.blade.php
+++ resources/views/user/wallet.blade.php
@@ -27,15 +27,15 @@
                           
                             <div class="row">
                             <div class="col-sm-12 col-md-4">
-                              <label for="">Available Balance</label>
-                              <h3 style="padding:10px;"> <span class="badge badge-primary">₦ {{ number_format($wallet,2)}}</span> </h3>
+                              {{-- <label for="">Available Balance</label> --}}
+                              <h4 style="padding:10px;">Available Balance <span class="badge badge-primary">₦ {{ number_format($wallet,2)}}</span> </h4>
                             </div>
                             <div class="col-sm-12 col-md-4">
                               {{-- margin-top: 20px; object-fit:contain; width:100%; height:42px; --}}
                               <img style="padding:10px;" src="{{ asset('assets/password/images/paystack.png')}}" class="img-responsive img-fluid" alt="paystack logo">
                             </div>
                             <div class="col-sm-12 col-md-4">
-                            <a href="{{ route('customer.top.up') }}" name="button" class="btn btn-primary " style="text-decoration:none; padding:10px;">&nbsp;<i class="fas fa-plus-circle"></i> Top Up</a>
+                            <a href="{{ route('customer.top.up') }}" name="button" class="btn btn-primary " style="text-decoration:none; margin:10px;">&nbsp;<i class="fas fa-plus-circle"></i> Top Up</a>
                             {{-- withdraw-button --}}
                             </div>
                           </div>

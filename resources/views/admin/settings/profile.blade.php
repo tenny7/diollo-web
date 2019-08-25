f431461737440e9fa5f0dc3dd44eac4d1c424f80
Ovie Tennyson <atomictenny@gmail.com>
2019-08-25 04:57:19 +0100

    Modified Setting UI in admin dashboard

--- resources/views/admin/settings/profile.blade.php
+++ resources/views/admin/settings/profile.blade.php
@@ -30,17 +30,17 @@
                                     <!-- Nav -->
                                     <ul class="nav nav-tabs nav-overflow header-tabs">
                                         <li class="nav-item">
-                                            <a href="#!" class="nav-link active">
+                                            <a href="{{route('admin.settings.profile')}}" class="nav-link active">
                                               Profile
                                             </a>
                                         </li>
                                         <li class="nav-item">
-                                            <a href="#!" class="nav-link">
+                                            <a href="{{route('admin.settings.password')}}" class="nav-link">
                                               Password
                                             </a>
                                         </li>
                                         <li class="nav-item">
-                                            <a href="#!" class="nav-link">
+                                            <a href="{{route('admin.settings.bank')}}" class="nav-link">
                                               Bank
                                             </a>
                                         </li>
